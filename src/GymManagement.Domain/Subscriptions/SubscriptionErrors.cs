5ffed34904414c223ad2354bec480f7a7b2e6bca
octav1an <octav1an@hotmail.com>
2024-04-05 15:34:09 +0200

    fix error description

--- src/GymManagement.Domain/Subscriptions/SubscriptionErrors.cs
+++ src/GymManagement.Domain/Subscriptions/SubscriptionErrors.cs
@@ -5,6 +5,6 @@ namespace GymManagement.Domain.Subscriptions;
 public static class SubscriptionErrors
 {
   public static readonly Error CannotHaveMoreGymsThanTheSubscriptionAllows = Error.Validation(
-    code: "Subscription.CannotHaveMoreGymsThanTheSubscriptionAllows",
-    description: "A subscription cannot have more gyms than the subscription allows");
+      code: "Subscription.CannotHaveMoreGymsThanTheSubscriptionAllows",
+      description: "A subscription cannot have more gyms than the subscription allows");
 }
